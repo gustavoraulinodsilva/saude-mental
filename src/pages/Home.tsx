26c086a8f93241e4ed64536f96926ec881b59d7f
Gustavo Raulino da Silva <gustavoraulinodasilva@gmail.com>
2025-03-23 16:23:14 -0300

    A página home está sendo carregada por um json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,84 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import "../sass/pages/_home.scss";
 
+//Interfaces
+interface HeroData{
+  image: string;
+  title: string;
+  desc: string;
+  cta: {
+    label: string;
+    link: string;
+  }
+}
+
+interface AboutData{
+  title: string;
+  text: string[]; 
+}
+
+interface HomeContent{
+  hero: HeroData;
+  about: AboutData;
+}
+
 const Home: React.FC = () => {
+
+  // Estado para armazenar os dados carregados
+  const [content, setContent] = useState<HomeContent | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('/src/data/home.json')
+    .then((response) => {
+      if(!response.ok){
+        throw new Error("Erro ao carregar JSON");
+      }
+      return response.json();
+    })
+    .then((data: HomeContent) => {
+      setContent(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError(error.message);
+      setLoading(false);
+    });
+  }, []);
+
+  if(loading){
+    return <div>Carregando....</div>;
+  }
+
+  if(error){
+    return <div>Erro: {error}</div>;
+  }
+
   return (
     <div className="home">
       <section className="hero">
         <div className="hero-banner">
-          <img src="/src/assets/images/SafeSpace-Saude-Mental-no-Trabalho-Porque-Precisamos-Falar-Sobre-02.png" alt="" />
+          <img src={content?.hero.image} alt="Banner" />
           <div className="banner-overlay"></div>
         </div>
         <div className="hero-content container">
-          <h1 className="hero-title">Cuidando da Mente, Transformando Vidas</h1>
+          <h1 className="hero-title">{content?.hero.title}</h1>
           <p className="hero-text">
-            Informações confiáveis e recursos para seu bem-estar emocional
+            {content?.hero.desc}
           </p>
-          <a href="#sobre-projeto" className="cta-button">
-            Saiba Mais
+          <a href={content?.hero.cta.link} className="cta-button">
+            {content?.hero.cta.label}
           </a>
         </div>
       </section>
       <section id="sobre-projeto" className="about-section container">
-        <h2 className="section-title">Sobre o Projeto</h2>
+        <h2 className="section-title">{content?.about.title}</h2>
         <div className="about-grid">
           <div className="about-text">
-            <p>
-              Desenvolvido por estudantes de engenharia e relações
-              internacionais em parceria com a Faculdade FSG, este projeto visa
-              democratizar o acesso à informação qualificada sobre saúde mental.
-            </p>
+            {content?.about.text.map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+            ))}
           </div>
         </div>
       </section>
